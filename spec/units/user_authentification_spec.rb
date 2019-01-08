95e3dd8e03ca8718c26290f05f6ed45322ca5345
Jacques <jacques.laffy@gmail.com>
2019-01-08 15:27:12 +0000

    Fixed Unit Tests

--- spec/units/user_authentification_spec.rb
+++ spec/units/user_authentification_spec.rb
@@ -1,21 +1,18 @@
 require 'User'
 
 describe User do
-  let(:user) {User.new}
   
   it "should return nil if user does not exist" do
     expect(User.authenticate("blabla@gmail.com", "somethign")).to eq(nil)
   end 
 
   it "should return nil if user exists but password is incorrect" do
-    allow(user).to receive(:email) {"something@gmail.com"}
-    allow(user).to receive(:password) {"somethingspecific"}
-    expect(User.authenticate("something@gmail.com", "somethingelse")).to eq(nil)
+    user = User.create(email: "somethingesle@gmail.com", password: "aaaa")
+    expect(User.authenticate("somethinelseg@gmail.com", "somethingelse")).to eq(nil)
   end
 
   it "should return user if correct details" do  
-    allow(user).to receive(:email) {"something@gmail.com"}
-    allow(user).to receive(:password) {"somethingspecific"}
-    expect(User.authenticate("something@gmail.com", "somethingspecific")).to eq(nil)
+    user = User.create(email: "something@gmail.com", password: "somethingspecific", id:5)
+    expect(User.authenticate("something@gmail.com", "somethingspecific")).to eq(user)
   end
 end
